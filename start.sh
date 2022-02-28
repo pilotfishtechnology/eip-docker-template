5daa719bbee8141968b1bf3ce097c2dc17f89f19
Danie <ddekock@pilotfishtechnology.com>
2022-02-28 11:30:41 +0200

    Remove Scripts

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -it -d --rm --name eiplatform \
-  -p 8080:8080 \
-  my-custom-eiplatform-app
--- upgrade.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sh stop.sh
-docker rm eiplatform
-docker rmi pilotfishtechnology/eiplatform
-docker rmi my-custom-eiplatform-app
-docker pull pilotfishtechnology/eiplatform
-sh build.sh
-sh start.sh
